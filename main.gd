11f0090b0fc0678ed3d1122a7191cbefe3185352
Chamerli <fares.setbel@proton.me>
2025-07-29 13:49:46 +0100

    the bobber blahaj

--- main.gd
+++ main.gd
@@ -6,3 +6,6 @@ onready var Lure = get_node("/root/SulayreLure")
 func _ready():
 	# Fish
 	Lure.add_content(ID, "Blåhaj", "mod://Resources/blahaj_fish.tres")
+	
+	# Bobber
+	Lure.add_content(ID, "Blåhaj Bobber", "mod://Resources/blahaj_bobber.tres")
